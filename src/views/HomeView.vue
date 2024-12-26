914be9ec9913eb6e59c9b69f21b76f249161acd8
OrlinGichev <o.gichev.genik@gmail.com>
2024-12-26 12:28:17 +0200

    Първоначална настройка : Vue3 проект с Vite, Router  и Firebase
    Добавено:
    - Базова структура на проекта
    - Настройка на Vue Router с начални маршрути
    - Конфигурация на Firebase
    - Базова структура на папките за компоненти и изгледи

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Добре дошли в Recipe App</h1>
+  </div>
+</template>
+<script setup></script>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Регистрация</h1>
+  </div>
+</template>
+<script setup></script>
